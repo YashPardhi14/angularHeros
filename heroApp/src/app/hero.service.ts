a5cd1b84c6d735c02bdf44e0346edce4d6ac9786
YashPardhi14 <yapardhi@gmail.com>
2024-01-24 22:41:01 +0530

    3rd Push

---
+++ heroApp/src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heros';
+import { Hero } from './hero';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes():Hero[]{
+    return HEROES;
+  }
+
+
+  
+}
--- heroApp/src/app/heroes/heroes.component.ts
+++ heroApp/src/app/heroes/heroes.component.ts
@@ -3,15 +3,24 @@ import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heros';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe,FormsModule,CommonModule],
+  imports: [UpperCasePipe,FormsModule,CommonModule,HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
- heroes=HEROES;
+
+  
+ heroes:Hero[]=[];
+ selectedHero?:Hero;
+
+ onSelect(hero:Hero){
+  this.selectedHero=hero;
+ }
 }
