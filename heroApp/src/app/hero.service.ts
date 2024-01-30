6d97148d06d6b8049ac6507a383e92ce72b9c318
YashPardhi14 <yapardhi@gmail.com>
2024-01-30 15:30:38 +0530

    upto service

--- heroApp/src/app/hero.service.ts
+++ heroApp/src/app/hero.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heros';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
 
-  getHeroes():Hero[]{
-    return HEROES;
+  getHeroes():Observable<Hero[]>{
+    const heroes=of(HEROES);
+this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 
 
--- heroApp/src/app/heroes/heroes.component.ts
+++ heroApp/src/app/heroes/heroes.component.ts
@@ -1,26 +1,57 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heros';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
-
+import { HttpClient } from '@angular/common/http';
+import { MessageService } from '../message.service';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe,FormsModule,CommonModule,HeroDetailComponent],
+  imports: [UpperCasePipe,FormsModule,CommonModule,HeroDetailComponent,MatProgressSpinnerModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
+  providers:[HttpClient]
 })
-export class HeroesComponent {
-
+export class HeroesComponent implements OnInit{
+constructor(private heroService:HeroService,private messageService: MessageService){}
   
- heroes:Hero[]=[];
- selectedHero?:Hero;
+  heroes :Hero[]|undefined=[];
 
- onSelect(hero:Hero){
-  this.selectedHero=hero;
- }
+  arrayList:number[]=[];
+
+  //#####Old Way....
+  // getHeroes():void{
+  //   this.heroes=this.heroService.getHeroes();
+  // }
+
+  getHeroes():void{
+     this.heroService.getHeroes().subscribe(
+      heroes=>this.heroes=heroes
+     )
+
+     /*
+The new version waits for the Observable to emit the array of heroes, 
+which could happen now or several minutes from now. 
+The subscribe() method passes the emitted array to the callback, 
+which sets the component's heroes property.
+
+     */
+  }
+
+  ngOnInit():void{
+this.getHeroes();
+
+  }
+
+  selectedHero!: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 }
